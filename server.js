0e276a8c3495c7445ffbc1ba95690416d1fe2a98
dargavavid <dargavavid@gmail.com>
2018-01-31 16:21:35 +0100

    Add projects template, modify header

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs');
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to fetch data'
